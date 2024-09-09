30863a4ec6b6d19b5063d0b084bed38abd519f35
Lina <linak0977@gmail.com>
2024-09-09 14:00:40 +0700

    Menambahkan file motor

--- database/migrations/2024_09_09_032655_create_motors_table.php
+++ database/migrations/2024_09_09_032655_create_motors_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('model');
             $table->integer('tahun');
             $table->integer('price');
+            $table->integer("stok");
             $table->timestamps();
         });
     }
